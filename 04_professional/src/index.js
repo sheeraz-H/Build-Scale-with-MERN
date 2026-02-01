aedda83bb8d4eada223b850b31fd73234551b21c
sheeraz-H <your-github-xherazusain@gmail.com>
2026-01-31 21:18:36 -0800

    connected with DB

---
+++ 04_professional/src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+// import mongoose from "mongoose"
+// import { DB_NAME } from "./constants";
+
+import connectDB from "./db/index.js";
+
+dotenv.config()
+
+
+connectDB()
+
+
+// first approach
+/*
+import express from "express"
+const app = express()
+
+( async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    app.on("error", (error) => {
+      console.log("ERR: ", error);
+      throw error
+    })
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`)
+    })
+  } catch (error) {
+    console.error("EROR: ", error)
+    throw error
+    
+  }
+})()
+
+*/
